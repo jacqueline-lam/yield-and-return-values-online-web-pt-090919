7ced882422be589f34d0a7937c47cec037f22e77
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-19 22:53:50 +0000

    Done.

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -2,11 +2,26 @@ require 'pry'
 
 def hello(array)
   i = 0
+  collection = [] #set varaible collection = empty array 
+  
   while i < array.length
-    yield(array[i])
+    collection << yield(array[i]) #push return value of using yeild(array[i]) into colelction array
     i += 1
   end
+
+  collection #return new collection at the end
 end
 
 
-hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+
+hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
+# Hi, Tim
+# Hi, Tom
+# Hi, Jim
+# => nil
+# return value of while loop is always nil — 
+# if we want our method to return something else, we have to tell it to do so 
+
+# We can capture the return value of the code that is executed when yield passes a value to a block
+# Calling yield(some_argument) will give us return value of execution of blcok with that argument
+# we can capture those return values
