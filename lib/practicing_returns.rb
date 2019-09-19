05dfa1d4d650f12150e94438fc8271a540caaffe
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-19 22:42:25 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -17,3 +17,5 @@ hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
 # => nil
 # return value of while loop is always nil — 
 # if we want our method to return something else, we have to tell it to do so 
+
+#We can capture the return value of the code that is executed when yield passes a value to a block.
